fa6f439b5deea0e8a6ef5c3dd57432bdb6078e33
xksteven <progressive_o@yahoo.com>
2014-04-25 14:46:33 -0400

    made simple client (does nothing atm)

--- amdi/proj4/battleship-client.c
+++ amdi/proj4/battleship-client.c
@@ -1,24 +1,47 @@
 //Creators Ana Diez and Steven Basart
 
 //Libraries to include
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
+#include "battleship.h"
 
 //--Predefined
-#define PORT 3333
 
 //Declare the functions
 //void
 
+int convertPortToInt(int port) {
+  char c;
+  int index, size = strlen(DEF_PORT);
+
+  index = 0;
+  while (size > 0) {
+    c = DEF_PORT[size];
+    port = (c - '0')*(index *10);
+    index++;
+    size--;
+  }
+  printf("port = %d\n",port);
+
+}
+
 
 //The starting point
 int main() {
+  int port, sockfd;
+  static struct sockaddr_in server_addr;
 
-  printf()
+  printf("Getting ready to play Battleship\n");
+
+  port = convertPortToInt(port);
+
+  //Try to establish a connection
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  if (sockfd < 0) {
+    perror("Socket failed to connect");
+    exit(1);
+  }
+
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
 
 }
