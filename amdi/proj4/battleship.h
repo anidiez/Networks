3c961a4443d6ee053eb4909c6eb3d80989d8e141
ana <anidiez@gmail.com>
2014-04-26 16:15:00 -0400

    changes to header file

--- amdi/proj4/battleship.h
+++ amdi/proj4/battleship.h
@@ -16,18 +16,15 @@
 #include<netdb.h>
 #include<fcntl.h>
 
-#define DEF_PORT 3335
+#define DEF_PORT 3333
 //MAXBUFLEN 4 bytes of header  + 512 bytes data + null terminate
 #define MAXBUFLEN 517
-//when putting data into stuff
-#define MAXDATA 512
 
 typedef enum {
-RRQ = 1,
-WRQ = 2,
-DATA = 3,
-ACK = 4,
-ERROR = 5
+HIT = 1,
+SHIP = 2,
+GAME_DATA = 3,
+ERROR = 4,
 } packet_type;
 
 #endif
